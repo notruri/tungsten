cefb1ce2702f165e2616d9cb65cf3a6b248761f5
ruri <ruriyoshinova@proton.me>
2026-03-13 20:21:43 +0800

    chore (gui/build): move import

--- crates/gui/build.rs
+++ crates/gui/build.rs
@@ -1,11 +1,9 @@
-use std::path::PathBuf;
-
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     println!("cargo:rerun-if-changed=assets/icons/tungsten.ico");
 
     #[cfg(target_os = "windows")]
     {
-        let manifest_dir = PathBuf::from(std::env::var("CARGO_MANIFEST_DIR")?);
+        let manifest_dir = std::path::PathBuf::from(std::env::var("CARGO_MANIFEST_DIR")?);
         let icon_path = manifest_dir.join("assets/icons/tungsten.ico");
         let icon_path = icon_path.to_string_lossy().into_owned();
 
